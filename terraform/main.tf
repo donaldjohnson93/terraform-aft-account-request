c61733c07ebb79c3eaa2792fe01728bde4fc7e05
Donald Johnson <donaldjohnson@Donalds-MacBook-Pro.local>
2025-02-18 21:52:35 -0600

    Importing Organization account

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "organization" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "donaldjohnson93+organization@protonmail.com"
+    AccountName               = "organization"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "donaldjohnson93+organization@protonmail.com"
+    SSOUserFirstName          = "Organization"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
